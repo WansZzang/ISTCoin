81962b09a0e91a55874edbadaa50da8c36de516f
WansZzang <jerry10247@gmail.com>
2019-03-18 23:10:26 +0900

    Update ISTCoin.sol

--- contracts/ISTCoin.sol
+++ contracts/ISTCoin.sol
@@ -1,7 +1,5 @@
 pragma solidity ^0.4.24;
 
-// File: contracts/YOACoin.sol
-
 library SafeMath {
     function mul(uint256 a, uint256 b) internal pure returns (uint256) {
         if (a == 0) {
@@ -27,6 +25,7 @@ library SafeMath {
         return c;
     }
 }
+
 contract Ownable {
     address public owner;
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
@@ -43,6 +42,7 @@ contract Ownable {
         owner = newOwner;
     }
 }
+
 contract ERC20Basic {
     function totalSupply() public view returns (uint256);
     function balanceOf(address who) public view returns (uint256);
@@ -50,6 +50,7 @@ contract ERC20Basic {
 
     event Transfer(address indexed from, address indexed to, uint256 value);
 }
+
 contract ERC20 is ERC20Basic {
     function allowance(address owner, address spender) public view returns (uint256);
     function transferFrom(address from, address to, uint256 value) public returns (bool);
@@ -57,140 +58,49 @@ contract ERC20 is ERC20Basic {
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-contract BasicToken is ERC20Basic, Ownable {
+contract BasicToken is ERC20Basic{
     using SafeMath for uint256;
     mapping(address => uint256) balances;
-    uint256 public totalSupply_;
-    uint256 public privatePreSale;
-    uint256 public openPreSale;
-    uint256 public openSale;
-
-    uint256 public ICOstarttime = 1541030400;
-    uint256 public ICOendtime =   1543536000;
-    //1541030400   //2018.11.1
-    //1543536000   //2018.11.30
+    uint256 internal totalSupply_;
     /**
-    * @dev total number of tokens in existence
-    */
+     * @dev total number of tokens in existence
+     */
     function totalSupply() public view returns (uint256) {
         return totalSupply_;
     }
 
-    function ICOactive() public view returns (bool success) {
-       return ICOstarttime < now && now < ICOendtime;
+    function _transfer(address _from, address _to, uint256 _value) internal returns (bool) {
+        require(_to != address(0));
+        require(_from != address(0));
+        require(_value != 0);
+        require(_value <= balances[_from]);
+        // SafeMath.sub will throw if there is not enough balance.
+        balances[_from] = balances[_from].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        emit Transfer(_from, _to, _value);
+        return true;
     }
+
     /**
     * @dev transfer token for a specified address
     * @param _to The address to transfer to.
     * @param _value The amount to be transferred.
-    */
-    function transfer(address _to, uint256 _value) public returns (bool) {
-        require(_to != address(0));
-        require(_value <= balances[msg.sender]);
-	if(msg.sender == owner){
-            if(now >= 1533128400 && now < 1534337940){
-                privatePreSale = privatePreSale.sub(_value);
-            } else if(now >= ICOstarttime && now < ICOendtime){
-                openPreSale = openPreSale.sub(_value);
-            } else if( ICOendtime < now ){
-                openSale = openSale.sub(_value);
-            }else{
-		return;
-	    }
-        }
+     */
 
-        // SafeMath.sub will throw if there is not enough balance.
-        balances[msg.sender] = balances[msg.sender].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        emit Transfer(msg.sender, _to, _value);
-        return true;
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        return _transfer(msg.sender, _to, _value);
     }
     /**
-    * @dev Gets the balance of the specified address.
-    * @param _owner The address to query the the balance of.
-    * @return An uint256 representing the amount owned by the passed address.
-    */
+     * @dev Gets the balance of the specified address.
+     * @param _owner The address to query the the balance of.
+     * @return An uint256 representing the amount owned by the passed address.
+     */
     function balanceOf(address _owner) public view returns (uint256 balance) {
         return balances[_owner];
     }
 }
 contract StandardToken is ERC20, BasicToken {
     mapping(address => mapping(address => uint256)) internal allowed;
-    
-    // list of receiver accounts
-    address[] public receivers;
-    bool private unFreeze = true;
-
-    mapping (address => bool) public frozenAccount;
-
-    event FrozenFunds(address target, bool frozen);
-    event AccountFrozenError();
-
-    function freezeAccount(address target, bool freeze) onlyOwner public {
-        frozenAccount[target] = freeze;
-        emit FrozenFunds(target, freeze);
-    }
-
-    // unfreeze all accounts
-    function unFreezeAll() public onlyOwner {
-        unFreeze = true;
-    }
-
-    function transfer(address _to, uint256 _value ) public returns (bool) {
-        // source account should not be frozen if contract is in not open state
-        if (frozenAccount[msg.sender] && !unFreeze) {
-            emit AccountFrozenError();
-            return false;
-        }
-        
-        // transfer fund first if sender is not frozen
-        require(super.transfer(_to, _value), "Transfer failed.");
-
-        // record the receiver address into list
-        receivers.push(_to);
-        
-        // automatically freeze receiver that is not whitelisted
-        if (frozenAccount[_to] == false && !unFreeze) {
-            frozenAccount[_to] = true;
-            emit FrozenFunds(_to, true);
-        }
-        return true;
-    }
-
-    function transferOwner(address _to, uint256 _value , uint256 _type) public returns (bool) {
-	if(msg.sender == owner){
-	    //type opensale
-            if(_type  == 1){
-                privatePreSale = privatePreSale.sub(_value);
-            } else if(_type  == 2){
-                openPreSale = openPreSale.sub(_value);
-            } else{
-		return;
-	    }
-        }
-	else{
-	    return;
-	}
-        // source account should not be frozen if contract is in not open state
-        if (frozenAccount[msg.sender] && !unFreeze) {
-            emit AccountFrozenError();
-            return false;
-        }
-        
-        // transfer fund first if sender is not frozen
-        require(super.transfer(_to, _value), "Transfer failed.");
-
-        // record the receiver address into list
-        receivers.push(_to);
-        
-        // automatically freeze receiver that is not whitelisted
-        if (frozenAccount[_to] == false && !unFreeze) {
-            frozenAccount[_to] = true;
-            emit FrozenFunds(_to, true);
-        }
-        return true;
-    }
-
 
     /**
      * @dev Transfer tokens from one address to another
@@ -199,34 +109,30 @@ contract StandardToken is ERC20, BasicToken {
      * @param _value uint256 the amount of tokens to be transferred
      */
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
-        require(_to != address(0));
-        require(_value <= balances[_from]);
         require(_value <= allowed[_from][msg.sender]);
-        require (!ICOactive());
-        require(!frozenAccount[_from]&& !unFreeze);                     // Check if sender is frozen
-        require(!frozenAccount[_to]&& !unFreeze);                       // Check if recipient is frozen
-
-        balances[_from] = balances[_from].sub(_value);
-        balances[_to] = balances[_to].add(_value);
+        _transfer(_from, _to, _value);
         allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
-        emit Transfer(_from, _to, _value);
         return true;
     }
+
     /**
-     * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
-     *
-     * Beware that changing an allowance with this method brings the risk that someone may use both the old
-     * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
-     * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
-     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-     * @param _spender The address which will spend the funds.
-     * @param _value The amount of tokens to be spent.
+    * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
+    *
+    * Beware that changing an allowance with this method brings the risk that someone may use both the old
+    * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
+    * race condition is to first reduce the spenders allowance to 0 and set the desired value afterwards:
+    * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+    * @param _spender The address which will spend the funds.
+    * @param _value The amount of tokens to be spent.
      */
+
     function approve(address _spender, uint256 _value) public returns (bool) {
+        require(_spender != address(0));
         allowed[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
     }
+
     /**
      * @dev Function to check the amount of tokens that an owner allowed to a spender.
      * @param _owner address The address which owns the funds.
@@ -247,13 +153,14 @@ contract StandardToken is ERC20, BasicToken {
      * @param _addedValue The amount of tokens to increase the allowance by.
      */
     function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
+        require(_spender != address(0));
+        require(_addedValue != 0);
         allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
         emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
         return true;
     }
     /**
      * @dev Decrease the amount of tokens that an owner allowed to a spender.
-     *
      * approve should be called when allowed[_spender] == 0. To decrement
      * allowed value is better to use this function to avoid 2 calls (and wait until
      * the first transaction is mined)
@@ -262,6 +169,8 @@ contract StandardToken is ERC20, BasicToken {
      * @param _subtractedValue The amount of tokens to decrease the allowance by.
      */
     function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
+        require(_spender != address(0));
+        require(_subtractedValue != 0);
         uint oldValue = allowed[msg.sender][_spender];
         if (_subtractedValue > oldValue) {
             allowed[msg.sender][_spender] = 0;
@@ -272,36 +181,70 @@ contract StandardToken is ERC20, BasicToken {
         return true;
     }
 }
-contract BurnableToken is BasicToken {
+
+contract BurnableToken is StandardToken {
     event Burn(address indexed burner, uint256 value);
     /**
-     * @dev Burns a specific amount of tokens.
-     * @param _value The amount of token to be burned.
-     */
+    * @dev Burns a specific amount of tokens.
+    * @param _value The amount of token to be burned.
+    */
     function burn(uint256 _value) public {
         require(_value <= balances[msg.sender]);
+        require(_value != 0);
         // no need to require value <= totalSupply, since that would imply the
         // sender's balance is greater than the totalSupply, which *should* be an assertion failure
-        address burner = msg.sender;
-        balances[burner] = balances[burner].sub(_value);
+        balances[msg.sender] = balances[msg.sender].sub(_value);
         totalSupply_ = totalSupply_.sub(_value);
-        emit Burn(burner, _value);
+        emit Transfer(msg.sender, address(0), _value);
+        emit Burn(msg.sender, _value);
     }
 }
 
-contract ISTCoin is StandardToken, BurnableToken {
+contract ISTCoin is BurnableToken, Ownable {
     string public constant name = "ISTCoin";
     string public constant symbol = "IST";
     uint8 public constant decimals = 8;
+
+    bool public unFreeze = false;
+    mapping (address => bool) public frozenAccount;
+
+    modifier notFrozen(address sender) {
+        require(!frozenAccount[sender] || unFreeze);
+        _;
+    }
+
+    event FrozenFunds(address target, bool frozen);
+    event FreezeAll(bool flag);
+    event AccountFrozenError();
 
     uint256 public constant INITIAL_SUPPLY = 200000000 * (10 ** uint256(decimals));
     constructor () public {
         totalSupply_ = INITIAL_SUPPLY;
         balances[msg.sender] = INITIAL_SUPPLY;
-        privatePreSale = 20000000 * (10 ** uint256(decimals));
-        openPreSale = 70000000 * (10 ** uint256(decimals));
-        openSale = 110000000 * (10 ** uint256(decimals));
         emit Transfer(0x0, msg.sender, INITIAL_SUPPLY);
+    }
 
+    function freezeAccount(address target, bool freeze) public onlyOwner {
+        frozenAccount[target] = freeze;
+        emit FrozenFunds(target, freeze);
+    }
+
+    // unfreeze all accounts
+    function unFreezeAll(bool flag) public onlyOwner {
+        unFreeze = flag;
+        emit FreezeAll(flag);
+    }
+
+    function transfer(address _to, uint256 _value) public notFrozen(msg.sender) returns (bool) {
+        return super.transfer(_to, _value);
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public notFrozen(_from) returns (bool) {
+        return super.transferFrom(_from, _to, _value);
+    }
+
+    function approve(address _spender, uint256 _value) public notFrozen(msg.sender) returns (bool) {
+        return super.approve(_spender, _value);
     }
 }
+
